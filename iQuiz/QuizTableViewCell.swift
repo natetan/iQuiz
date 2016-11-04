83cc13c51f6d591172f4fc232a59b4f3dcf91ef8
Yulong Tan <yulongtan67@gmail.com>
2016-11-03 21:28:46 -0700

    mass update

---
+++ iQuiz/QuizTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  QuizTableViewCell.swift
+//  iQuiz
+//
+//  Created by iGuest on 11/3/16.
+//  Copyright © 2016 yulongproductions. All rights reserved.
+//
+
+import UIKit
+
+class QuizTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descLabel: UILabel!
+    
+    @IBOutlet weak var iconImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        iconImage.image = UIImage(named: "quiz-icon")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    public func setImage(imageName: String) {
+        iconImage.image = UIImage(named: imageName)
+    }
+    
+}
