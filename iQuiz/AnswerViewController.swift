9ae3f1507c3b97f11cfd629ef80ce3a36ab6279a
Yulong Tan <yulongtan67@gmail.com>
2016-11-08 20:32:11 -0800

    view controller for answer

---
+++ iQuiz/AnswerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AnswerViewController.swift
+//  iQuiz
+//
+//  Created by iGuest on 11/8/16.
+//  Copyright © 2016 yulongproductions. All rights reserved.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
